2e7569b0ce21522c53d0e9ecd4aefbd14a9e9603
BenoitPE <benoitpegaz@gmail.com>
2022-10-10 16:48:21 +0200

    Ajout de la fonction pure
    Co-Authored-By: acleret69 <72598333+acleret69@users.noreply.github.com>

--- Programmation_Fonctionnelle/Programmation_Fonctionnelle/Program.cs
+++ Programmation_Fonctionnelle/Programmation_Fonctionnelle/Program.cs
@@ -4,38 +4,45 @@
     {
         public static void Main()
         {
-            while(true)
+            while (true)
             {
                 Console.WriteLine(">--------------------------------------------<");
                 Console.WriteLine("Veuillez sélectionner un principe de Programmation fonctionnelle");
-                Console.WriteLine("\t- 0: Fonction d'ordre supérieur");
-                Console.WriteLine("\t- 1: Fonction pure");
-                Console.WriteLine("\t- 2: Idempotence");
-                Console.WriteLine("\t- 3: Immutabilité");
-                Console.WriteLine("\t- 4: Composition de fonction (pure et impure");
-                Console.WriteLine("\t- 5: Composition Over inheritance");
-                Console.WriteLine("\t- 6: Séparation entre domaines purs et impurs");
+                Console.WriteLine("\t- 1: Fonction d'ordre supérieur");
+                Console.WriteLine("\t- 2: Fonction pure");
+                Console.WriteLine("\t- 3: Idempotence");
+                Console.WriteLine("\t- 4: Immutabilité");
+                Console.WriteLine("\t- 5: Composition de fonction (pure et impure");
+                Console.WriteLine("\t- 6: Composition Over inheritance");
+                Console.WriteLine("\t- 7: Séparation entre domaines purs et impurs");
                 Console.WriteLine(">--------------------------------------------<");
+                
+                int value = 0;
+                if (int.TryParse(Console.ReadLine(), out value))
+                    Environment.Exit(0);
 
-                int value = Convert.ToInt32(Console.ReadLine());
                 Console.Clear();
                 switch (value)
                 {
                     case 0:
+                    default:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
                         HigherOrderFunctions.Run();
                         break;
                     case 2:
+                        PureFunction.Run();
+                        break;
+                    case 3:
                         Idempotency.Run();
                         break;
-                    default:
-                        Console.Clear();
-                        break;
+
                 }
                 Console.WriteLine("\nAPPUYEZ SUR ENTRER POUR REVENIR AU MENU");
                 Console.ReadLine();
                 Console.Clear();
             }
-
         }
     }
 }
---
+++ Programmation_Fonctionnelle/Programmation_Fonctionnelle/PureFunction.cs
@@ -0,0 +1,24 @@
+﻿using Programmation_Fonctionnelle.Common;
+
+namespace Programmation_Fonctionnelle
+{
+    public static class PureFunction
+    {
+        public static void Run()
+        {
+            Copyright.print("Fonction pure",
+                "Une fonction pure est une fonction sans effet de bord, qui s'occupe uniquement de renvoyer une valeur, ne dépend que de ses paramètres d'entrée sans les modifier",
+                "Ici Multiply() est une fonction pure car elle n'a pas d'effet de bord, elle retourne une valeur et ne modifie par les paramètres d'entrée");
+            int a = 3;
+            int b = 4;
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            Console.WriteLine($"a * b = {Multiply(a, b)}");
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
